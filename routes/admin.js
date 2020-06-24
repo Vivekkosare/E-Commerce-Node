d2ee0ba8b88b3a3a2657053cdfad944801d97e19
Vivek Kosare <v_kosare@yahoo.com>
2020-06-24 20:50:45 +0200

    Added ejs template files

--- routes/admin.js
+++ routes/admin.js
@@ -20,8 +20,6 @@ router.get('/add-product', (req, res, next) => {
 })
 
 router.post('/add-product', (req, res, next) => {
-    // console.log(req.body);
-    // res.redirect('/');
     products.push({
         title: req.body.title
     });
