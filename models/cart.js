e39d81fccc16a8c6ca7129c686ee050556037d0f
Vivek Kosare <v_kosare@yahoo.com>
2020-07-17 22:10:46 +0200

    implemented cart and add to cart

--- models/cart.js
+++ models/cart.js
@@ -1,27 +1,39 @@
 const fs = require('fs');
 const path = require('path');
 const util = require('../util/path');
+const { createDecipher } = require('crypto');
 const p = path.join(util, 'data', 'cart.json');
 
 module.exports = class Cart{
     static addProduct(id, productPrice){
         fs.readFile(p, (err, fileContent) => {
-            let cart = {products : [], totalPrice : 0};
-
-            if(!err){
+            let cart = {products: [], totalPrice: 0};
+            console.log('fileContent: ', fileContent);
+            // const fileData = JSON.parse(fileContent);
+            if(!err && fileContent != undefined){
                 cart = JSON.parse(fileContent);
             }
-            const existingProduct = cart.products.find(prod => prod.id === id);
-            let updatedProduct ;
+
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+            const existingProduct = cart.products[existingProductIndex];
+            let updatedProduct;
+
             if(existingProduct){
                 updatedProduct = {...existingProduct};
                 updatedProduct.qty = updatedProduct.qty + 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
             }
             else{
-                updatedProduct = {id : id, qty: 1};
+                updatedProduct = {id: id, qty: 1};
+                cart.products = [...cart.products, updatedProduct];
             }
+            console.log('value: ', parseFloat(productPrice));
+            cart.totalPrice = cart.totalPrice + +parseFloat(productPrice);
 
-            cart.totalPrice = cart.totalPrice + productPrice;
+            fs.writeFile(p, JSON.stringify(cart), (err) => {
+                console.log(err);
+            });
         });
     }
 };
