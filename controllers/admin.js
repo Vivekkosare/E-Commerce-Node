6f29046e285eb1725fda4ede81c6c02bbb64b25c
Vivekkosare <vivek.kosare@barnebys.com>
2020-07-19 00:16:17 +0200

    added getEditCart method

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,9 @@
 const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        activeProduct: true,
-        formCSS: true,
-        productCSS: true
+        editing: false
     });
 };
 
@@ -18,6 +16,28 @@ exports.postAddProduct = (req, res, next) => {
     console.log('save called');
     product.save();
     res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if(!editMode){
+        return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId, product =>{
+        if(!product){
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product',{
+            pageTitle: 'Edit Product',
+            path: 'admin/edit-product',
+            editing: editMode,
+            product: product
+        });
+
+    });
+
+    
 };
 
 exports.getProducts = (req, res, next) => {
